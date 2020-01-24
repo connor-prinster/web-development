7946c0a8363377335cdd0bafde1771da3224c37a
Connor Prinster <connor.prinster@gmail.com>
2020-01-24 11:19:03 -0700

    fixed things somewhat?

--- game-dev/gameLoop/design/main.js
+++ game-dev/gameLoop/design/main.js
@@ -1,16 +1,16 @@
 let events = []
 let validEvents = []
 
-document.onkeydown = function (e) {
+document.onkeydown = function(e) {
     e = e || window.event;
     switch (e.which || e.keyCode) {
-          case 13 : //Your Code Here (13 is ascii code for 'ENTER')
-              submitButtonPress()
+        case 13: //Your Code Here (13 is ascii code for 'ENTER')
+            submitButtonPress()
     }
 }
 
 window.onload = function() {
-    
+
     let submitButton = this.document.getElementById("submitButton")
     submitButton.onclick = () => {
         this.submitButtonPress()
@@ -20,10 +20,21 @@ window.onload = function() {
 }
 
 function reportToNode(event) {
-    let eventsNode = this.document.getElementById("eventText")
-    event.times--
-    eventText = ("Event: " + event.name + " (" + event.times + " remaining)")
-    eventsNode.innerHTML += (eventText + "<br>")
+    // === decrement the event before writing === //
+    event.times--;
+
+    // === Generate the individual nodes === //
+    let singleEventNode = document.createElement("div")
+    let eventText = document.createTextNode("Event: " + event.name + " (" + event.times + " remaining)")
+    let br = document.createElement("br")
+    singleEventNode.appendChild(eventText)
+    singleEventNode.appendChild(br)
+
+    // === Get and Append Value to Screen === //
+    let eventsNode = this.document.getElementById("eventArticle")
+    eventsNode.insertBefore(singleEventNode, eventsNode.childNodes[0])
+
+    // === event was modified, return it === //
     return event
 }
 
@@ -38,25 +49,24 @@ function submitButtonPress() {
     let interval = this.parseInt(intervalNode.value)
     let times = this.parseInt(timesNode.value)
 
-    // === Reset Values === //
+    // === Reset Values on Inputs === //
     nameNode.value = ""
     intervalNode.value = null
     timesNode.value = null
 
     // === Create an Event Object === //
-    if(nameNode != "" && !this.isNaN(interval) && !this.isNaN(times)) {
+    if (nameNode != "" && !this.isNaN(interval) && !this.isNaN(times)) {
         let event = {
             "nextCall": 0,
-            "name": name, 
-            "interval": interval, 
+            "name": name,
+            "interval": interval,
             "times": times
         }
         this.reportToNode(event)
         event.nextCall = (event.interval + performance.now())
         console.log("createdEvent:", event)
         events.push(event)
-    }
-    else {
+    } else {
         this.alert("Something was wrong with your input")
     }
 }
@@ -77,19 +87,18 @@ function gameLoop(elapsedTime) {
 }
 
 function processInput(elapsedTime) {
-    
+
 }
 
 function update(elapsedTime) {
-    for(let i = 0; i < events.length; i++) {
+    for (let i = 0; i < events.length; i++) {
         const event = events[i]
-        if(event.times > 0) {
-            if(elapsedTime >= event.nextCall) {
+        if (event.times > 0) {
+            if (elapsedTime >= event.nextCall) {
                 events.pop()
                 validEvents.push(event)
             }
-        }
-        else {
+        } else {
             events.pop()
         }
     }
@@ -97,7 +106,7 @@ function update(elapsedTime) {
 }
 
 function render() {
-    while(validEvents.length > 0) {
+    while (validEvents.length > 0) {
         events.push(reportToScreen(validEvents.pop()))
     }
 }
