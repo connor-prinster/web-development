21241dc2df410d8f7f6bc088586b6f29b0044115
Connor Prinster <connor.prinster@gmail.com>
2020-03-02 18:26:58 -0700

    clockhands is done

---
+++ interview/Qualtrics/scrambledPalindrome.js
@@ -0,0 +1,4 @@
+// Is Scrambled Palindrome
+// Write a function that, given a string of letters, returns true or false for whether the letters in the string
+// could be arranged to form a palindrome.
+// E.g. For “torro”, the answer is True, because the letters can be rearranged to spell “rotor”.
