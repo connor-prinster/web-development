215e9f2171173d2652fe8f0e82a3d369deed6634
Connor Prinster <connor.prinster@gmail.com>
2020-03-23 23:16:54 -0600

    initialized the new branch to begin a new assignment

---
+++ multi-agent/vickreyAuctions/main.js
@@ -0,0 +1,12 @@
+let names = []
+let votes = []
+let preloadedValues = []
+let dataset = ""
+
+window.onload = () => {
+    this.initialize()
+}
+
+function initialize() {
+    console.log("initialize")
+}
