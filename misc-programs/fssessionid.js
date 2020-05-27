41ae5d6ebcea3fded97f8ec26ffb1ee838e6a416
Connor Prinster <connorprinster@Connors-MacBook-Pro.local>
2020-05-27 12:12:45 -0600

    working on stuff, not necessarily sure anymore. I saved it a long time ago

--- misc-programs/fssessionid.js
+++ misc-programs/fssessionid.js
@@ -25,3 +25,36 @@ let main = () => {
 };
 
 main();
+
+javascript:(
+  function() {
+    let main = () => {
+      const el = document.createElement('textarea');
+      el.value = ""
+
+      const cookie = document.cookie.split(';');
+      for(let i = 0; i < cookie.length; i++) {
+        let splitString = cookie[i].split("=");
+        const key = splitString[0].trim();
+        const value = splitString[1].trim();
+        if(key == "fssessionid") {
+          el.value = value;
+        }
+      }
+
+      if(el.value === "") {
+        return false;
+      }
+
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      
+      alert("Copied fssessionid to clipboard")
+      return true;
+    }
+    
+    main()
+  }
+)()
