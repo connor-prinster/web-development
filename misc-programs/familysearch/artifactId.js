b48e4427426ed804d1a4e476613812c799ebd43b
Connor Prinster <connorprinster@Connors-MacBook-Pro.local>
2020-06-04 17:01:40 -0600

    after an embarrasing failure, this should fix everything?

--- misc-programs/familysearch/artifactId.js
+++ misc-programs/familysearch/artifactId.js
@@ -1,30 +1,49 @@
 javascript: (
     function () {
         let main = event => {
+            // set up the event listener
             document.removeEventListener("copy", main, true);
             event.preventDefault();
             let clipboardData = event.clipboardData;
+            // set the document in focus
             const el = document.createElement('textarea');
             el.value = "";
-
-            let artifactId = 
-                document.getElementsByTagName("gallery-app")[0].
+            // there is a potential issue here. if you are on the 
+            // wrong page, everything dies with a null issue. to
+            // remove the chance of that, I put a try/catch
+            let children = ""
+            try {
+                children = 
+                    document.getElementsByTagName("gallery-app")[0].
                     shadowRoot.children.modalViewer.
                     shadowRoot.children.artifactViewerModal.
                     children.header.children["header-container"].
-                    children["expandIconMobile"].href
-                    .split("/artifacts/")[1].split("?")[0];
-
-            clipboardData.clearData();
-            clipboardData.setData("text/plain", artifactId)
-            document.body.appendChild(el);
-            el.select(); 
-            document.body.removeChild(el);
-            alert("Copied artifactId=" + artifactId + " to clipboard");
+                    children;
+            }
+            catch(e) {
+                alert("Something went wrong. Are you on the right page?", e);
+                return;
+            }
+            // depending on the size of the webpage, the button holding
+            // the artifactId will either be "expandIcon" or "expandIconMobile".
+            // this ternary expression should fix this
+            let correctChild = children["expandIconMobile"] === undefined ? "expandIcon" : "expandIconMobile";
+            let artifactId = "";
+            try {
+                artifactId = children[correctChild].href.split("/artifacts/")[1].split("?")[0];
+                clipboardData.clearData();
+                clipboardData.setData("text/plain", artifactId);
+                document.body.appendChild(el);
+                el.select(); 
+                document.body.removeChild(el);
+                alert("Copied artifactId=" + artifactId + " to clipboard");
+            }
+            catch(e) {
+                alert("Something went wrong. Are you on the right page?", e);
+                return;
+            }
         };
         document.addEventListener("copy", main, true);
-        document.execCommand("copy")
+        document.execCommand("copy");
     }
-  )()
-
-// javascript: (function () %7Blet main %3D event %3D> %7Bdocument.removeEventListener("copy", main, true)%3Bevent.preventDefault()%3Blet clipboardData %3D event.clipboardData%3Bconst el %3D document.createElement('textarea')%3Bel.value %3D ""%3Blet artifactId %3D document.getElementsByTagName("gallery-app")[0].shadowRoot.children.modalViewer.shadowRoot.children.artifactViewerModal.children.header.children["header-container"].children["expandIconMobile"].href.split("/artifacts/")[1].split("?")[0]%3BclipboardData.clearData()%3BclipboardData.setData("text/plain", artifactId)%3Bdocument.body.appendChild(el)%3Bel.select()%3B document.body.removeChild(el)%3Balert("Copied artifactId%3D" %2B artifactId %2B " to clipboard")%3B}%3Bdocument.addEventListener("copy", main, true)%3Bdocument.execCommand("copy")%3B})()
+)()
