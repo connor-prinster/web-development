543bedf4aa3e2c9588255704b2487558ef5f0506
Connor Prinster <connor.prinster@familysearch.org>
2020-08-07 17:25:14 -0600

    fading colors exist now too

--- misc-programs/familysearch/dashboard_colors.js
+++ misc-programs/familysearch/dashboard_colors.js
@@ -1,7 +1,25 @@
 var c = document.querySelector("table").querySelectorAll("td > div"); 
 function co() { 
     c.forEach(({ style }, l) => { 
-            l < 12 && (style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)) 
+        let splitArr = style.cssText.split("; ")
+        style.cssText = ""
+        if(style && splitArr.length === 7) {
+            splitArr.push("opacity: 30%")
         }
-    )
+        else if(splitArr.length === 8) {
+        
+            let opacity = parseInt(splitArr[7].split(": ")[1].split("%")[0])
+            if(opacity < 1) {
+                opacity += Math.random()
+            }
+            else {
+                opacity -= Math.random
+            }
+            splitArr[7] = "opacity: " + opacity
+        }
+
+        for(let i in splitArr) {
+            style.cssText += splitArr[i]
+        }
+    })
 } setInterval(co, 300);
